8d81ca2d645b70990d00018291472695a0744ae9
otkken2 <lovelovezamakko2@gmail.com>
2025-03-13 02:39:30 +0900

    first commit(認証機能が一通りできたところまで)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,6 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import ConfigureAmplifyClientSide from "@/components/ConfigureAmplify";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={``}>
+        <ConfigureAmplifyClientSide />
         {children}
       </body>
     </html>
---
+++ middleware.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { fetchAuthSession } from "aws-amplify/auth/server";
+
+import { runWithAmplifyServerContext } from "@/utils/amplify-utils";
+
+export async function middleware(request: NextRequest) {
+  const response = NextResponse.next();
+
+  const authenticated = await runWithAmplifyServerContext({
+    nextServerContext: {request, response},
+    operation: async (contextSpec) => {
+      try {
+        const session = await fetchAuthSession(contextSpec, {});
+        return session.tokens !== undefined;
+      } catch(error) {
+        console.log(error);
+        return false;
+      }
+    }
+  });
+
+  if (authenticated) {
+    return response;
+  }
+
+  return NextResponse.redirect(new URL("/login", request.url));
+}
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|login).*)",
+  ]
+}
